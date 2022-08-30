239935df26b26cbcd8b5b3a3219601c166229b18
Brady Stillman <bestillmanUNO@gmail.com>
2022-08-30 00:26:18 +0000

    added my major

--- hello.c
+++ hello.c
@@ -9,5 +9,8 @@ int main(int argc, char **argv) {
     printf("Hello, I am Reagan Bourgeois!\n");
     printf("My major is Electrical Engineering!\n");
 
+    printf("What's up dweebs I'm Brady Stillman!\n")
+    printf("I'm also majoring in Computer Engineering\n")
+
     return 0;
 }
